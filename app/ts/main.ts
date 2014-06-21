ae110780bb57f34d73d40449dbfa0f6cf6a4d642
masahirompp <masahirompp@gmail.com>
2014-06-21 14:08:45 +0900

    aaa

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,20 +1,32 @@
 /// <reference path="d3.d.ts" />
 
-var dataset : any = [10,20,30,40];
+var width : number = document.documentElement.clientWidth;
+var height : number = document.documentElement.clientHeight;
+
+var dataset : any = [10];
 
 var $svg : any = d3.select('.container-fluid').append('svg');
 
-$svg.selectAll('circle')
+$svg.selectAll('ellipse')
 .data(dataset)
 .enter()
-.append('circle')
+.append('ellipse')
 .attr({
-  cx: function(d, i){
-    return i * 100 + 50;
-  },
-  cy: 100,
-  r: function(d){
-    return d;
-  },
+  cx: width / 2,
+  cy: height / 2,
+  rx: 200,
+  ry: 100,
+  stroke: 'red',
+  'stroke-width': 5,
   fill: 'red'
-})
+});
+
+$svg.selectAll('text')
+.data(dataset)
+.enter()
+.append('text')
+.text('aaa')
+.attr({
+  x: width / 2,
+  y: height / 2
+});
